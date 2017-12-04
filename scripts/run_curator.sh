21c45b1937815a8de05a0363dd7a69c0fe1279c9
jeanlouisboudart <jeanlouis.boudart@gmail.com>
2017-12-04 13:39:42 +0100

    use the file as is, don't append to it

--- scripts/run_curator.sh
+++ scripts/run_curator.sh
@@ -6,7 +6,7 @@ ACTION_FILE=/config/custom-actions.yml
 RANCHER_BASEURL="rancher-metadata.rancher.internal/latest"
 
 echo "Fetching curator custom actions"
-curl -sf ${RANCHER_BASEURL}/self/service/metadata/curator-actions >> ${ACTION_FILE}
+curl -sf ${RANCHER_BASEURL}/self/service/metadata/curator-actions > ${ACTION_FILE}
 
 echo "Executing curator"
 curator --config ${CONFIG_FILE} ${ACTION_FILE}
